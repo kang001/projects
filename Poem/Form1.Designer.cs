07a2531441c995b2d5d6b94836273fbd0a7e2b15
gs6296gc <gs6296gc@T301025-1625308>
2016-08-31 18:54:11 -0500

    Add project files.

---
+++ Poem/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Poem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSeed = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblSeed = new System.Windows.Forms.Label();
+            this.lblTreeOutput = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSeed
+            // 
+            this.txtSeed.Location = new System.Drawing.Point(172, 114);
+            this.txtSeed.Name = "txtSeed";
+            this.txtSeed.Size = new System.Drawing.Size(100, 20);
+            this.txtSeed.TabIndex = 0;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(172, 140);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(100, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "&Generate Poem ";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblSeed
+            // 
+            this.lblSeed.AutoSize = true;
+            this.lblSeed.Location = new System.Drawing.Point(12, 121);
+            this.lblSeed.Name = "lblSeed";
+            this.lblSeed.Size = new System.Drawing.Size(95, 13);
+            this.lblSeed.TabIndex = 2;
+            this.lblSeed.Text = "Enter Seed Word: ";
+            // 
+            // lblTreeOutput
+            // 
+            this.lblTreeOutput.AutoSize = true;
+            this.lblTreeOutput.Location = new System.Drawing.Point(12, 199);
+            this.lblTreeOutput.Name = "lblTreeOutput";
+            this.lblTreeOutput.Size = new System.Drawing.Size(98, 13);
+            this.lblTreeOutput.TabIndex = 3;
+            this.lblTreeOutput.Text = "[Word Output here]";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Instructions: ";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 428);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 463);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTreeOutput);
+            this.Controls.Add(this.lblSeed);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtSeed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSeed;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblSeed;
+        private System.Windows.Forms.Label lblTreeOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Poem/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Net;
+/* Rules of the program:
+ * For now, a poem will be 4 lines
+ * Figure out how to generate a random sentence 
+ * //http://stackoverflow.com/questions/23346853/random-sentence-generator-listbox-output
+ * 
+*/
+namespace Poem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            /*Figure out how to format a very long string
+             * or figure out if I want different sorts of 
+             * outpits for each of the lines
+             * ALSO: figure out general rules for the application
+            */
+            //test for a program that creates random text 
+            string[] article = { "the", "a", "one", "some", "any", };
+            string[] noun = { "boy", "girl", "dog", "town", "car", };
+            string[] verb = { "drove", "jumped", "ran", "walked", "skipped", };
+            string[] preposition = { "to", "from", "over", "under", "on", };
+
+            /*Another idea would be to have different types of words
+             * and the random class would pull random types of words from their list
+             * and would string them together to make some sort of sentence structure 
+             * the apis would feed into these lists of the words 
+            */
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { //on form load, send api request to rhyme brain 
+            WebRequest request = WebRequest.Create("http://rhymebrain.com/talk?function=getRhymes&word=kitten");
+            WebResponse resp = request.GetResponse();
+            Console.Write(resp);
+
+            Stream data = resp.GetResponseStream();
+
+            StreamReader reader = new StreamReader(data);
+
+            string jsonwehope = reader.ReadToEnd();
+            //maxResults
+            /*this api has a request parameter that allows the user 
+             * to choose however many rhyming words they want returned
+            */
+
+            //more comments 
+            int randomVariable = 5; 
+            //more comments
+
+
+            // DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(WebResponse));
+
+            //TODO Deal with JSON somehow?
+
+
+            Console.Write(jsonwehope);
+            resp.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
