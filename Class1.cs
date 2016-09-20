3828ba0e25cb59c42e01091128e4dc752ab793bc
cats <cats@jellybeans.com>
2016-09-20 16:04:14 -0500

    added a class

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        GetHashCode; AppDomainSetup; 
+        //add a class that takes the jason object and feeds it out to the outgoing class here 
+	}
+}
